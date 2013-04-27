8f233e3cf927c1df2e1d9c85141251f1e6dc7014
Elton Baroncello <eltonbaroncello@gmail.com>
2013-04-27 11:52:22 -0300

    Lista 2 Exercício 5 - Ordenar valores inteiros em um vetor.
    Leia um A vetor (10) de inteiros. Ordene, a seguir, os elementos de A em
    ordem crescente e escreva o vetor ordenado.
    
    verão 0.1 - não compilado/testado

---
+++ l2p5-vetorordenado.c
@@ -0,0 +1,41 @@
+/*
+	Exercício: 05
+	Data: 27/04/2013
+	Autor: Elton Baroncello
+	Descrição: Leia um A vetor (10) de inteiros. Ordene, a seguir, os elementos de A em ordem crescente e escreva o vetor ordenado.
+
+	Falta testar
+	
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define TAM 10
+
+int main(){
+	int i, j, a[TAM], ax;
+	
+	for(i=0; i<TAM; i++){
+		printf("\nDigite um valor para A[%i]: ", i);
+		scanf("%i", &a[i]);
+	}
+	
+	// passa por cada indice [0=5,1=3,2=2,3=1,4=6,5=8,6=4,7=7,8=6,9=5]
+	for(i=0; i<TAM; i++){
+		// move o indice maior para a direita
+		for (j = 0; j < TAM; ++j){
+			if(a[j]>=a[j-1]){
+				ax = a[j-1];
+				a[j-1] = a[j];
+				a[j] = ax;
+			}
+		}
+	}
+
+	printf("\n\nVetore Ordenados\n");
+	for (i = 0; i < TAM; ++i){
+		printf("A[%i] = %i\n", i, a[i]);
+	}
+	
+	return 0;
+}
