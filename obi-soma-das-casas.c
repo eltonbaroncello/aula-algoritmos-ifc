6563c0f9ddbd45d28b094fe0c4137a7a8595ddf0
Elton Baroncello <eltonbaroncello@gmail.com>
2013-06-17 11:26:26 -0300

    Create obi-soma-das-casas.c
    /*
    	Exercício: OBI2012, Fase 2, Nível 2 - Soma de casas
    	Data: 17/06/2013
    	Autor: Elton Baroncello
    	Descrição:	
    
    Joãozinho mora em uma rua que tem N casas. Marquinhos é o melhor amigo dele, mas sempre gosta de pregar peças em Joãozinho. Desta vez, ele pegou os dois brinquedos prediletos de Joãozinho e os escondeu em duas casas distintas da rua. Em compensação, Marquinhos deu uma dica importante para Joãozinho:
    
    A soma dos números das casas em que escondi teus brinquedos é igual a K. Além disso, escolhi as casas de tal forma que não existe outro par de casas cuja soma tenha esse mesmo valor.
    
    Sabendo disto, encontre qual é o par de casas em que se encontram os brinquedos de Joãozinho. Para auxiliar seu amigo, Marquinhos entregou a Joãozinho uma lista com o número das casas já em ordem crescente (isto é, do menor para o maior número).
    
    Entrada
    
    A primeira primeira linha da entrada contém um número inteiro N, que representa o número de casas que existem na rua. Cada uma das N linhas seguintes contém um número inteiro, representando o número de uma casa. Note que esses N números estão ordenados, do menor para o maior. A última linha da entrada contém um inteiro K, que é a soma dos números das duas casas onde os brinquedos estão escondidos.
    
    Saída
    
    Se programa deve imprimir uma única linha, contendo dois inteiros, A e B, A < B, que representam os números das casas em que estão escondidos os brinquedos. Os dois números devem ser separados por um espaço em branco.
    
    Informações sobre a pontuação
    
    Em um conjunto de casos que totaliza 30 pontos, N ≤ 103.
    Restrições
    
    2 ≤ N ≤ 105
    Para cada casa Ci, 0 ≤ Ci ≤ 109, i = 1, 2, ... , N
    Os números das casas estão em ordem crescente, do menor para o maior número, e casas distintas têm números distintos.
    
    Entrada
    4
    1
    2
    3
    5
    8
    
    Saída
    3 5
    
    OU
    
    Entrada
    4
    1
    2
    3
    5
    5
    
    Saída
    2 3
    
    */

---
+++ obi-soma-das-casas.c
@@ -0,0 +1,64 @@
+/*
+  Exercício: OBI2012, Fase 2, Nível 2 - Soma de casas
+	Data: 17/06/2013
+	Autor: Elton Baroncello
+	Descrição:	
+
+Joãozinho mora em uma rua que tem N casas. Marquinhos é o melhor amigo dele, mas sempre gosta de pregar peças em Joãozinho. Desta vez, ele pegou os dois brinquedos prediletos de Joãozinho e os escondeu em duas casas distintas da rua. Em compensação, Marquinhos deu uma dica importante para Joãozinho:
+
+A soma dos números das casas em que escondi teus brinquedos é igual a K. Além disso, escolhi as casas de tal forma que não existe outro par de casas cuja soma tenha esse mesmo valor.
+
+Sabendo disto, encontre qual é o par de casas em que se encontram os brinquedos de Joãozinho. Para auxiliar seu amigo, Marquinhos entregou a Joãozinho uma lista com o número das casas já em ordem crescente (isto é, do menor para o maior número).
+
+Entrada
+
+A primeira primeira linha da entrada contém um número inteiro N, que representa o número de casas que existem na rua. Cada uma das N linhas seguintes contém um número inteiro, representando o número de uma casa. Note que esses N números estão ordenados, do menor para o maior. A última linha da entrada contém um inteiro K, que é a soma dos números das duas casas onde os brinquedos estão escondidos.
+
+Saída
+
+Se programa deve imprimir uma única linha, contendo dois inteiros, A e B, A < B, que representam os números das casas em que estão escondidos os brinquedos. Os dois números devem ser separados por um espaço em branco.
+
+Informações sobre a pontuação
+
+Em um conjunto de casos que totaliza 30 pontos, N ≤ 103.
+Restrições
+
+2 ≤ N ≤ 105
+Para cada casa Ci, 0 ≤ Ci ≤ 109, i = 1, 2, ... , N
+Os números das casas estão em ordem crescente, do menor para o maior número, e casas distintas têm números distintos.
+
+Entrada
+4
+1
+2
+3
+5
+8
+
+Saída
+3 5
+
+OU
+
+Entrada
+4
+1
+2
+3
+5
+5
+
+Saída
+2 3
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define N 4
+
+int main(){
+	int n[N], k, i;
+	
+	return 0;
+}
